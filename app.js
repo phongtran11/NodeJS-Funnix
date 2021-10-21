f0fec8d94f954993433f2de04c92048d4aad5f6e
Phong Tran <phongtran11.tt@gmail.com>
2021-10-21 21:12:36 +0700

    lab2.8

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const shopRouter = require('./router/shop');
 
 app.use(bodyParser.urlencoded({extended: true})) ;
 
-app.use(adminRouter);
 app.use(shopRouter);
+app.use(adminRouter);
 
-
+app.use((req, res) => {
+    res.status(404).send('<h1>Page Not Found</h1>')
+})
 
 app.listen(3000, console.log(`App running at http://localhost:3000`));
