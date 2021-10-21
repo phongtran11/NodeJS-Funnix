fee541a487af6e57b56beb66845f9dd072ec4c1f
Phong Tran <phongtran11.tt@gmail.com>
2021-10-21 22:01:29 +0700

    lab2.12

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const bodyParser = require('body-parser');
 const adminRouter = require('./router/admin');
 const shopRouter = require('./router/shop');
@@ -10,7 +11,7 @@ app.use('/admin',adminRouter);
 app.use(shopRouter);
 
 app.use((req, res) => {
-    res.status(404).send('<h1>Page Not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(3000, console.log(`App running at http://localhost:3000`));
