84a2f64014ea1e78080cf4fe7f6c61516e032977
Phong Tran <phongtran11.tt@gmail.com>
2021-10-21 17:23:43 +0700

    lab2.1

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require('http');
+const express = require('express');
+const app = express();
 
-const routes = require('./routes');
  
-const server = http.createServer(routes);
+const server = http.createServer(app);
 
 server.listen(3000);
