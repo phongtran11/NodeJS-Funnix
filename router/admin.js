05e86a41d9a114486a4b708e1c5320eb1aad2195
Phong Tran <phongtran11.tt@gmail.com>
2021-10-21 21:55:19 +0700

    lab2.11

--- router/admin.js
+++ router/admin.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 // GET /admin/add-product
 router.get('/add-product',(req, res, next) => {
-    res.send('<form action="/admin/add-product" method="POST"><input name="title" type="text"><button type="submit">Submit</button></form>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 })
 
 // POST /admin/add-product
