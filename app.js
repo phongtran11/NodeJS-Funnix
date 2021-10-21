3a28102024d07a490620ab3af0eeb21017612eb5
Phong Tran <phongtran11.tt@gmail.com>
2021-10-21 17:37:45 +0700

    lab2.3

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use((req, res, next) => {
  
 app.use((req, res, next) => {
     console.log('Another middleware');
+    res.send('<h1>Hello World</h1>');
 })
 
 const server = http.createServer(app);
