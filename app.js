cf0a314cdd88b5b62ef5cfaef2f4eb1ac3d08cb3
Phong Tran <phongtran11.tt@gmail.com>
2021-10-21 19:46:17 +0700

    lab2.4

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Middleware');
+app.use('/', (req, res, next) => {
+    console.log('This always run');
     next();
-})
- 
-app.use((req, res, next) => {
+});
+
+app.use('/add-product',(req, res, next) => {
     console.log('Another middleware');
+    res.send('<h1>The Add product page</h1>');
+})
+
+app.use('/',(req, res, next) => {
+    console.log('Middleware');
     res.send('<h1>Hello World</h1>');
 })
 
