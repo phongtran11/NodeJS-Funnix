9e873f15850ee33ca7a0dc425b6b5f5ee8767573
Phong Tran <phongtran11.tt@gmail.com>
2021-10-21 20:29:32 +0700

    lab2.6

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.use('/', (req, res, next) => {
-    console.log('This always run');
-    next();
-});
+app.use(bodyParser.urlencoded({extended: true})) ;
 
 app.use('/add-product',(req, res, next) => {
-    console.log('Another middleware');
-    res.send('<h1>The Add product page</h1>');
+    res.send('<form action="/product" method="POST"><input name="title" type="text"><button type="submit">Submit</button></form>');
+})
+
+app.post('/product', (req, res, next) =>{
+    console.log(req.body)
+    res.redirect('/');
 })
 
 app.use('/',(req, res, next) => {
-    console.log('Middleware');
     res.send('<h1>Hello World</h1>');
 })
 
-app.listen(3000);
+app.listen(3000, console.log(`App running at http://localhost:3000`));
