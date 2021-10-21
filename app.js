e253be935de8288755894bb6b36714597590edb4
Phong Tran <phongtran11.tt@gmail.com>
2021-10-21 17:31:31 +0700

    lab2.1

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('Middleware');
+    next();
+})
  
+app.use((req, res, next) => {
+    console.log('Another middleware');
+})
+
 const server = http.createServer(app);
 
 server.listen(3000);
