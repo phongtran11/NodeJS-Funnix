0d10eaef2847da46c90de09a2c5732ba5a80807e
Phong Tran <phongtran11.tt@gmail.com>
2021-10-22 10:20:29 +0700

    lab3.2

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 const adminRouter = require('./router/admin');
 const shopRouter = require('./router/shop');
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({extended: true})) ;
 app.use(express.static(path.join(__dirname, 'public')));
 
