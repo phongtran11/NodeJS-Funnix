461f949cd31a9684c323728c60e414d3a85510c3
Phong Tran <phongtran11.tt@gmail.com>
2021-10-23 20:30:27 +0700

    lab3.7

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 const adminRouter = require('./router/admin');
 const shopRouter = require('./router/shop');
 
-app.set('view engine', 'pug');
-app.set('views', 'views');
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
+
 
 app.use(bodyParser.urlencoded({extended: true})) ;
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,7 +17,8 @@ app.use('/admin',adminRouter.router);
 app.use(shopRouter);
 
 app.use((req, res) => {
-    res.status(404).render('404', {pageTitle: 'Page not found'});
+    res.status(404);
+    res.render('404', {pageTitle: 'Page not found'});
 })
 
 app.listen(3000, console.log(`App running at http://localhost:3000`));
