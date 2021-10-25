37957dcdfb2b6964b3de896d3b0df72fefa11114
Phong Tran <phongtran11.tt@gmail.com>
2021-10-25 19:01:06 +0700

    lab4.7

--- router/shop.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ProductControllers = require('../controllers/products');
-
-// GET /
-router.get('/', ProductControllers.getProduct);
-
-module.exports = router
