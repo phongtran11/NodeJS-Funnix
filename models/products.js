07a4b1918b91c74c77dee5c1670d5820d330deae
Phong Tran <phongtran11.tt@gmail.com>
2021-10-25 17:33:49 +0700

    lab4.6

--- models/products.js
+++ models/products.js
@@ -1,40 +1,38 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'product.json'
+);
+
+const getProductFormFile = (callback) => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) { 
+           callback([]);
+        } else {
+            callback(JSON.parse(fileContent)) ;
+        }
+    });
+}
+
 module.exports = class Product {
     constructor(t) {
         this.title = t;
     };
     
     save() {
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            'data',
-            'product.json'
-        );
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductFormFile((products) => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), err => {
                 console.log(err);
             })
-        });
+        })
+      
     };
 
-    static fetchAll(callback) {
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            'data',
-            'product.json'
-        );
-        fs.readFile(p, (err, fileContent) => {
-            if (err) { 
-                callback([])
-            }
-            callback(JSON.parse(fileContent)) ;
-        });
+    static fetchAll(cb) {
+        getProductFormFile(cb);
     };
 };
