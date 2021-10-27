1eca2bab5d8cfda06a0caaa61e06a6ae46dd0919
Phong Tran <phongtran11.tt@gmail.com>
2021-10-27 10:27:02 +0700

    lab4.16

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
@@ -39,7 +39,19 @@ exports.getEditProduct = (req, res, next) => {
 };
 
 exports.postEditProduct = (req, res, next) => {
-
+  const productId =  req.body.prodId;
+  const updateTitle = req.body.title;
+  const updatePrice = req.body.price;
+  const updateImageUrl = req.body.imageUrl;
+  const updateDescription = req.body.description;
+  const updateProduct = new Product(productId, updateTitle,updateImageUrl ,updateDescription  ,updatePrice );
+  console.log('title:', updateTitle,
+              'price:', updatePrice,
+              'description:',updateDescription,
+              updateImageUrl
+    )
+  updateProduct.save();
+  res.redirect('/admin/products');
 }
 
 exports.getProducts = (req, res, next) => {
