319111f2207199bb57062031faf5118d20932b3e
Phong Tran <phongtran11.tt@gmail.com>
2021-10-28 15:47:22 +0700

    lab5.8

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'nodejs',
-    password: '020899Pi'
-});
+const sequelize = new Sequelize(
+    'nodejs', 'root', '020899Pi',
+    {
+        dialect: 'mysql',
+        host: 'localhost',
+    });
 
-module.exports = pool.promise();
+module.exports = sequelize;
