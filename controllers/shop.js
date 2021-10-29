96b476f9f4340be693586a33cd911e7f20770dc3
Phong Tran <phongtran11.tt@gmail.com>
2021-10-29 10:09:44 +0700

    lab5.13

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,15 +14,26 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-  .then(([product]) => {
+  // Product.findAll({
+  //   where: {id : prodId}
+  // })
+  // .then((product) => {
+  //   res.render('shop/product-detail', {
+  //     product: product[0],
+  //     pageTitle: product[0].title,
+  //     path: '/products'
+  //   });
+  // })
+  // .catch(err => console.log(err));
+  Product.findByPk(prodId)
+  .then((product) => {
     res.render('shop/product-detail', {
-      product: product[0],
+      product: product,
       pageTitle: product.title,
       path: '/products'
     });
   })
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
