5eb8d565d63399a925b6db87fc546470a57ca0a1
Phong Tran <phongtran11.tt@gmail.com>
2021-10-31 15:49:31 +0700

    lab6.16

--- models/cart.js
+++ models/cart.js
@@ -1,13 +1,1 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../util/database');
-
-const Cart = sequelize.define('Cart', {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true,
-    },
-});
-
 module.exports = Cart;
--- models/user.js
+++ models/user.js
@@ -3,9 +3,11 @@ const getDb = require('../util/database').getDb;
 
 const ObjectId = mongodb.ObjectId;
 class User {
-    constructor(name, email) {
+    constructor(name, email, cart, id) {
         this.name = name;
         this.email = email;
+        this.cart = cart;
+        this._id = id;
     }
 
     save() {
@@ -17,6 +19,17 @@ class User {
             .catch((err) => {
                 console.log(err);
             });
+    }
+
+    addToCart(product) {
+        const updateProduct = { item: [{ ...product, quantity: 1 }] };
+        const db = getDb();
+        return db
+            .collection('users')
+            .updateOne(
+                { _id: new ObjectId(this._id) },
+                { $set: { cart: updateProduct } }
+            );
     }
 
     static findById(userId) {
