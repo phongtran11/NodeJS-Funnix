e556a97bb36084fed03714687375de75cf9f35ed
Phong Tran <phongtran11.tt@gmail.com>
2021-10-31 18:46:40 +0700

    lab6.20

--- controllers/shop.js
+++ controllers/shop.js
@@ -80,7 +80,7 @@ exports.postCart = (req, res, next) => {
     const prodId = req.body.productId;
     Product.findById(prodId)
         .then((product) => {
-            req.user.addToCart(product);
+            return req.user.addToCart(product);
         })
         .then((result) => {
             console.log('Add to cart successfully');
