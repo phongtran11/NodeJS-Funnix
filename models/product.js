822a7b3c6bb44401d15d93695b8fddf4d37e977c
Phong Tran <phongtran11.tt@gmail.com>
2021-11-01 09:53:55 +0700

    lab7.2

--- models/product.js
+++ models/product.js
@@ -1,3 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+    title: {
+        type: String,
+        require: true,
+    },
+    price: {
+        type: Number,
+        require: true,
+    },
+    description: {
+        type: String,
+        require: true,
+    },
+    imageUrl: {
+        type: String,
+        require: true,
+    },
+});
+
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
 
