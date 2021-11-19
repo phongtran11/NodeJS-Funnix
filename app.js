b9f02df8293c3caeb4ed672a9ffbfbb27e3c132a
Phong Tran <phongtran11.tt@gmail.com>
2021-11-19 14:03:04 +0700

    lab8.7

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://phong:020899Pi@cluster0.eirsr.mongodb.net/shop';
+
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions',
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -18,10 +25,17 @@ const authRoutes = require('./routes/auth');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false }));
+app.use(
+    session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+        store: store,
+    })
+);
 
 app.use((req, res, next) => {
-    User.findById('5bab316ce0a7c75f783cb8a8')
+    User.findById('617f64cc8d2425edac9d57d3')
         .then((user) => {
             req.user = user;
             next();
@@ -36,9 +50,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-    .connect(
-        'mongodb+srv://phong:020899Pi@cluster0.eirsr.mongodb.net/shop?authSource=admin&replicaSet=atlas-12dlbs-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true'
-    )
+    .connect(MONGODB_URI)
     .then((result) => {
         User.findOne().then((user) => {
             if (!user) {
