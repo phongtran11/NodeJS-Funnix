b0a20c3dd3f15b95822656cefb1c8b7c16f2d663
Phong Tran <phongtran11.tt@gmail.com>
2021-11-19 14:18:52 +0700

    lab8.8

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
     // const isLoggedIn = req.get('Cookie').split(';')[3].trim().split('=')[1] === 'true';
     res.render('auth/login', {
@@ -8,6 +10,19 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    req.session.isLoggedIn = true;
-    res.redirect('/');
+    User.findById('617f64cc8d2425edac9d57d3')
+        .then((user) => {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            res.redirect('/');
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(() => {
+        res.redirect('/');
+    });
 };
