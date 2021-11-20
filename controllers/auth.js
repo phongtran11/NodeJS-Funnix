a47f1ba86015395a845ae6ec1eebc1d2e3a6f2e0
Phong Tran <phongtran11.tt@gmail.com>
2021-11-20 09:08:20 +0700

    lab8.24

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,6 +6,7 @@ exports.getLogin = (req, res, next) => {
         path: '/login',
         pageTitle: 'Login',
         isAuthenticated: false,
+        errorMessage: req.flash('error'),
     });
 };
 
@@ -23,6 +24,7 @@ exports.postLogin = (req, res, next) => {
     User.findOne({ email: email })
         .then((user) => {
             if (!user) {
+                req.flash('error', 'Invalid email or password.');
                 return res.redirect('/login');
             }
             bcryptjs
