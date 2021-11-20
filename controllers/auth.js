2f62d880b739accf5561f139a4869ac8c0c8d747
Phong Tran <phongtran11.tt@gmail.com>
2021-11-20 10:28:51 +0700

    lab9.1

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,6 @@
 const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator/check');
+
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
@@ -70,6 +72,15 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
+    const errors = validationResult(req);
+    if (!errors.isEmpty) {
+        return res.status(422).render('auth/signup', {
+            path: '/signup',
+            pageTitle: 'Signup',
+            errorMessage: errors.array(),
+        });
+    }
+
     User.findOne({ email: email })
         .then((userDoc) => {
             if (userDoc) {
