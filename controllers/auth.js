190fc71758a19621a501e596e985d6e60dc671bd
Phong Tran <phongtran11.tt@gmail.com>
2021-11-20 09:23:48 +0700

    lab8.27

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,19 +2,32 @@ const bcryptjs = require('bcryptjs');
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
         isAuthenticated: false,
-        errorMessage: req.flash('error'),
+        errorMessage: message,
     });
 };
 
 exports.getSignup = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
     res.render('auth/signup', {
         path: '/signup',
         pageTitle: 'Signup',
         isAuthenticated: false,
+        errorMessage: message,
     });
 };
 
@@ -39,6 +52,7 @@ exports.postLogin = (req, res, next) => {
                             res.redirect('/');
                         });
                     }
+                    req.flash('error', 'Invalid email or password.');
                     res.redirect('/login');
                 })
                 .catch((error) => {
@@ -59,6 +73,7 @@ exports.postSignup = (req, res, next) => {
     User.findOne({ email: email })
         .then((userDoc) => {
             if (userDoc) {
+                req.flash('error', 'Email exists already.');
                 return res.redirect('/signup');
             }
             return bcryptjs
