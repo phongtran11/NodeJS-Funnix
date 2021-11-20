4b31b703c88773ec15acd19ee3f507db45ae8bda
Phong Tran <phongtran11.tt@gmail.com>
2021-11-20 09:19:01 +0700

    lab8.26

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,11 +2,17 @@ const bcryptjs = require('bcryptjs');
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
         isAuthenticated: false,
-        errorMessage: req.flash('error'),
+        errorMessage: message,
     });
 };
 
