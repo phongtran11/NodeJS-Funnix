32ea124e9fdfae419cc83fc122aba711f974eb49
Phong Tran <phongtran11.tt@gmail.com>
2021-11-22 09:23:33 +0700

    lab9.7

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,6 +30,11 @@ exports.getSignup = (req, res, next) => {
         pageTitle: 'Signup',
         isAuthenticated: false,
         errorMessage: message,
+        oldInput: {
+            email: '',
+            password: '',
+            confirmPassword: '',
+        },
     });
 };
 
@@ -72,11 +77,17 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
         return res.status(422).render('auth/signup', {
             path: '/signup',
             pageTitle: 'Signup',
             errorMessage: errors.array()[0].msg,
+            oldInput: {
+                email,
+                password,
+                confirmPassword: req.body.confirmPassword,
+            },
         });
     }
     bcryptjs
