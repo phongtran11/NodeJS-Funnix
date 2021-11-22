b9d63045d439aa0b91da28c9ce12273304d7c11e
Phong Tran <phongtran11.tt@gmail.com>
2021-11-22 10:34:08 +0700

    lab9.11

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const { validationResult } = require('express-validator/check');
 
 exports.getAddProduct = (req, res, next) => {
     if (!req.session.isLoggedIn) {
@@ -7,8 +8,9 @@ exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
+        hasError: false,
         editing: false,
-        isAuthenticated: req.session.isLoggedIn,
+        errorMessage: null,
     });
 };
 
@@ -17,6 +19,24 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(422).render('admin/edit-product', {
+            pageTitle: 'Add Product',
+            path: '/admin/edit-product',
+            editing: false,
+            hasError: true,
+            product: {
+                title,
+                imageUrl,
+                price,
+                description,
+            },
+            errorMessage: errors.array()[0].msg,
+        });
+    }
+
     const product = new Product({
         title: title,
         price: price,
@@ -52,7 +72,9 @@ exports.getEditProduct = (req, res, next) => {
                 path: '/admin/edit-product',
                 editing: editMode,
                 product: product,
+                hasError: false,
                 isAuthenticated: req.session.isLoggedIn,
+                errorMessage: null,
             });
         })
         .catch((err) => console.log(err));
